f19080f70bd518eef9cc4ca982bf9d39e72b22de
robbywashere <robertapolana@gmail.com>
2018-09-04 11:00:59 -0400

    whoops comma

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,58 +15,59 @@ module.exports = {
         anonymize: false,
         // Setting this parameter is also optional
         respectDNT: false,
-      },
-      {
-        resolve: `gatsby-plugin-favicon`,
-        options: {
-          logo: "./static/img/logo.svg.png",
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./static/img/logo.svg.png",
 
-          // WebApp Manifest Configuration
-          appName: 'Tiny Func',
-          appDescription: null,
-          developerName: null,
-          developerURL: null,
-          dir: 'auto',
-          lang: 'en-US',
-          background: '#fff',
-          theme_color: '#fff',
-          display: 'standalone',
-          orientation: 'any',
-          start_url: '/',
-          version: '1.0',
+        // WebApp Manifest Configuration
+        appName: 'Tiny Func',
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/',
+        version: '1.0',
 
-          icons: {
-            android: true,
-            appleIcon: true,
-            appleStartup: true,
-            coast: true,
-            favicons: true,
-            firefox: true,
-            opengraph: true,
-            twitter: true,
-            yandex: true,
-            windows: true
-          }
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: true,
+          favicons: true,
+          firefox: true,
+          opengraph: true,
+          twitter: true,
+          yandex: true,
+          windows: true
         }
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-next',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages/blog`,
+        name: 'blog',
       },
-      'gatsby-plugin-react-helmet',
-      'gatsby-plugin-styled-components',
-      'gatsby-plugin-react-next',
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/src/pages/blog`,
-          name: 'blog',
-        },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-pages`,
+        name: 'markdown-pages',
       },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/src/markdown-pages`,
-          name: 'markdown-pages',
-        },
-      },
-      `gatsby-transformer-remark`,
-      'gatsby-plugin-remove-trailing-slashes',
-    ],
-    }
+    },
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-remove-trailing-slashes',
+  ],
+}
